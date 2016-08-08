31959de461e843098c35f9821c6a0eb6d9bc2a5b
philoniare <philoniare@gmail.com>
2016-08-08 14:56:13 -0400

    feat: created a simple tableView to display stored bill amounts

--- Tip Calculator/HistoryViewController.swift
+++ Tip Calculator/HistoryViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 import RealmSwift
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var billTableView: UITableView!
+    var storedBills: Results<BillRecord>!
     
     override func viewWillAppear(animated: Bool) {
+        // populate the list with bills from storage
         let realm = try! Realm()
-        let allBills = realm.objects(BillRecord)
-        print(allBills)
+        storedBills = realm.objects(BillRecord)
+    }
+    
+    override func viewDidLoad() {
+         billTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.storedBills.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.billTableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel?.text = String(self.storedBills[indexPath.row].billAmount)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
 }
